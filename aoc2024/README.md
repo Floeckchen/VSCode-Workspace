6d51e6efa8922dd2af93f33948758620b2ead39f
Florian Thimm <florian.thimm@gambit.de>
2024-12-17 17:07:08 +0100

    mob programming day 8

--- aoc2024/README.md
+++ aoc2024/README.md
@@ -18,7 +18,7 @@
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
-![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
@@ -111,16 +111,16 @@ Both parts: 2586.727ms
 
 ```
 Day 07
-Time part 1: 78.901ms
-Time part 2: 2433.582ms
-Both parts: 2512.483ms
+Time part 1: 39.908ms
+Time part 2: 1347.536ms
+Both parts: 1387.445ms
 ```
 
 ```
 Day 08
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 5.794ms
+Time part 2: 5.698ms
+Both parts: 11.492ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 14/50
-Total time: 5379.519ms
+Total stars: 16/50
+Total time: 4265.974ms
 ```
 
 <!--/RESULTS-->
---
+++ aoc2024/src/day08/index.ts
@@ -0,0 +1,202 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+class solver {
+  private input: string;
+
+  constructor(input: string) {
+    this.input = input;
+  }
+
+  part1() {
+    const grid = this.input.split("\n").map( (line) => line.split(""));
+
+    const min_x = 0;
+    const min_y = 0;
+    const max_x = grid[0].length;
+    const max_y = grid.length;
+
+    const antennas : string[] = [];
+    let antinodes : Set<string> = new Set();
+
+    //Find all antennas
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] != "." && antennas.includes(grid[i][j]) == false) {
+          antennas.push(grid[i][j]);          
+        }
+      }
+    }
+
+    for (let i = 0; i < antennas.length; i++) {
+      let antenna = antennas[i];
+
+      for (let row = 0; row < grid.length; row++) {
+        for (let column = 0; column < grid[row].length; column++) {
+
+          if (grid[row][column] == antenna) {
+
+            for (let y = row; y < grid.length; y++) {
+              for (let x = 0; x < grid[y].length; x++) {
+
+                if ( y === row && x <= column ) {
+                  continue;
+                }
+
+                if (grid[y][x] == antenna) {
+                  let dist_x = column - x;
+                  let dist_y = row - y;
+
+                  let y1 = row + dist_y;
+                  let x1 = column + dist_x;
+
+                  let y2 = y - dist_y;
+                  let x2 = x - dist_x;
+
+                  if (!(y1 < min_y || y1 >= max_y || x1 < min_x || x1 >= max_x)) {
+                    antinodes.add(`${y1},${x1}`);
+                  }
+
+                  if (!(y2 < min_y || y2 >= max_y || x2 < min_x || x2 >= max_x)) {
+                    antinodes.add(`${y2},${x2}`);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return antinodes.size;
+  }
+
+  part2()  {
+    const grid = this.input.split("\n").map( (line) => line.split(""));
+
+    const min_x = 0;
+    const min_y = 0;
+    const max_x = grid[0].length;
+    const max_y = grid.length;
+
+    const antennas : string[] = [];
+    let antinodes : Set<string> = new Set();
+
+    //Find all antennas
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] != "." && antennas.includes(grid[i][j]) == false) {
+          antennas.push(grid[i][j]);          
+        }
+      }
+    }
+
+    for (let i = 0; i < antennas.length; i++) {
+      let antenna = antennas[i];
+
+      for (let row = 0; row < grid.length; row++) {
+        for (let column = 0; column < grid[row].length; column++) {
+
+          if (grid[row][column] == antenna) {
+
+            for (let y = row; y < grid.length; y++) {
+              for (let x = 0; x < grid[y].length; x++) {
+
+                if ( y === row && x <= column ) {
+                  continue;
+                }
+
+                if (grid[y][x] == antenna) {
+                  let dist_x = column - x;
+                  let dist_y = row - y;
+
+                  let y1 = row;
+                  let x1 = column;
+
+                  let y2 = y;
+                  let x2 = x;
+
+                  while (!(y1 < min_y || y1 >= max_y || x1 < min_x || x1 >= max_x)) {
+                    antinodes.add(`${y1},${x1}`);
+                    y1 += dist_y;
+                    x1 += dist_x;
+                  }
+
+                  while (!(y2 < min_y || y2 >= max_y || x2 < min_x || x2 >= max_x)) {
+                    antinodes.add(`${y2},${x2}`);
+                    y2 -= dist_y;
+                    x2 -= dist_x;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return antinodes.size;
+  }
+}
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return new solver(input).part1();
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return new solver(input).part2();
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+        ............
+        ........0...
+        .....0......
+        .......0....
+        ....0.......
+        ......A.....
+        ............
+        ............
+        ........A...
+        .........A..
+        ............
+        ............
+        `,
+        expected: 14,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+      input: `
+      ............
+      ........0...
+      .....0......
+      .......0....
+      ....0.......
+      ......A.....
+      ............
+      ............
+      ........A...
+      .........A..
+      ............
+      ............
+      `,
+      expected: 34,
+    },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
